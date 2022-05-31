06762a8e28a8b7554186897947a8efb3fb041bb8
soobnini <soobiny4339@gmail.com>
2022-06-01 03:07:32 +0900

    수정:DTO JPA버전으로 수정

---
+++ sharebook/src/main/java/com/sharebook/sharebook/domain/Community.java
@@ -0,0 +1,35 @@
+package com.sharebook.sharebook.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+@SuppressWarnings("serial")
+@Entity
+@Table(name="Member")
+@Data
+public class Community implements Serializable{
+	@Id
+	private int community_id;
+	private String title;
+	public int category;
+	private String content;
+	private int views;
+	private Date upload_date;
+	@ManyToOne
+	@JoinColumn(name="member_id")
+	private Member member;
+	
+	/*댓글 리스트
+	 * @OneToMany(mappedBy="community") private List<Comment> commentList;
+	 */
+	
+}
