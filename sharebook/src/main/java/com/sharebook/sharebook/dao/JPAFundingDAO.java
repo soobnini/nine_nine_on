04111be73b260e8f498c9601abd94782d4c5954a
mikaniz <k515jina@gmail.com>
2022-06-01 12:39:59 +0900

    Add DAO classes using JPA

---
+++ sharebook/src/main/java/com/sharebook/sharebook/dao/JPAFundingDAO.java
@@ -0,0 +1,74 @@
+package com.sharebook.sharebook.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.sharebook.sharebook.domain.Funding;
+
+@Repository
+public class JPAFundingDAO implements FundingDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public void create(Funding funding) throws DataAccessException {
+		em.persist(funding);
+	}
+
+	@Override
+	public void delete(int funding_id) throws DataAccessException {
+		em.remove(findFundingById(funding_id));
+	}
+
+	@Override
+	public void update(int funding_id, Funding funding) throws DataAccessException {
+		em.merge(funding);
+	}
+
+	@Override
+	public Funding findFundingById(int funding_id) throws DataAccessException {
+		return em.find(Funding.class, funding_id);
+	}
+
+	@Override
+	public List<Funding> findFundingByTitle(String title) throws DataAccessException {
+		TypedQuery<Funding> query = em.createQuery(
+				"select funding from FUNDING funding "
+				+ "where funding.title=?1", Funding.class);
+		query.setParameter(1, title);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Funding> findFundingByAuthor(String author) throws DataAccessException {
+		TypedQuery<Funding> query = em.createQuery(
+				"select funding from FUNDING funding "
+				+ "where funding.author=?1", Funding.class);
+		query.setParameter(1, author);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Funding> findFundingList() throws DataAccessException {
+		TypedQuery<Funding> query = em.createQuery(
+				"select funding from FUNDING funding ", Funding.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Funding> findFundingList(int user_id) throws DataAccessException {
+		TypedQuery<Funding> query = em.createQuery(
+				"select funding from FUNDING funding "
+				+ "where funding.user_id=?1", Funding.class);
+		query.setParameter(1, user_id);
+		return query.getResultList();
+	}
+
+}
