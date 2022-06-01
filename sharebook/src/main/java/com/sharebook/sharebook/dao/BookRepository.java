a4f08be8e9cde6c16389f291fcede91cd911b0ab
gs25q <rs13q5@gmail.com>
2022-06-01 16:56:00 +0900

    추가 : Book, Chat_room 관련 DTO 및 DAO 추가

---
+++ sharebook/src/main/java/com/sharebook/sharebook/dao/BookRepository.java
@@ -0,0 +1,21 @@
+package com.sharebook.sharebook.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sharebook.sharebook.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+	/*
+	 * Book Return Method 
+	 */
+	Book findBookByTitle(String title);
+	Book findBookByAuthor(String author);
+	
+	/*
+	 * List Return Method 
+	 */
+	List<Book> findBook();
+	List<Book> findBookByMember_id(int member_id);
+}
