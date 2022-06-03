fe262f280aa8bb619335946fd4f6359a4909f0b8
mikaniz <k515jina@gmail.com>
2022-06-04 01:44:08 +0900

    Fix parameter type

--- sharebook/src/main/java/com/sharebook/sharebook/repository/Funding_orderRepository.java
+++ sharebook/src/main/java/com/sharebook/sharebook/repository/Funding_orderRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sharebook.sharebook.domain.Funding_order;
 
 public interface Funding_orderRepository extends JpaRepository<Funding_order, Integer> {
-	List<Funding_order> findByFunding_id(String funding_id);
+	List<Funding_order> findByFunding_id(int funding_id);
 	
-	List<Funding_order> findByMember_id(String member_id);
+	List<Funding_order> findByMember_id(int member_id);
 }
