7238b1213ecf40876720eb83fd653efbf58eee41
mikaniz <k515jina@gmail.com>
2022-06-04 02:00:01 +0900

    Allow LIKE searches in Funding searches

--- sharebook/src/main/java/com/sharebook/sharebook/repository/FundingRepository.java
+++ sharebook/src/main/java/com/sharebook/sharebook/repository/FundingRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sharebook.sharebook.domain.Funding;
 
 public interface FundingRepository extends JpaRepository<Funding, Integer> {
-	List<Funding> findByTitle(String title);
+	List<Funding> findByTitleContaining(String title);
 	
-	List<Funding> findByAuthor(String author);
+	List<Funding> findByAuthorContaining(String author);
 	
 	List<Funding> findByMember_id(int member_id);
 }
