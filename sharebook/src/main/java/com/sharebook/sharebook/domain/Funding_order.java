0f4bbf41e00eb050ec060c1e9170dda8ff0236e5
mikaniz <k515jina@gmail.com>
2022-06-11 14:53:41 +0900

    Set domain classes for sequence

--- sharebook/src/main/java/com/sharebook/sharebook/domain/Funding_order.java
+++ sharebook/src/main/java/com/sharebook/sharebook/domain/Funding_order.java
@@ -2,29 +2,37 @@ package com.sharebook.sharebook.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor 
 @NoArgsConstructor
+@Entity
+@SequenceGenerator(name="FUNDING_ORDER_SEQ_GENERATOR",
+					sequenceName="FUNDING_ORDER_SEQ",
+					initialValue=1, allocationSize=1)
 @Table(name="FUNDING_ORDER")
 public class Funding_order {
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,
+					generator="FUNDING_ORDER_SEQ_GENERATOR")
 	@Column(name="FUNDING_ORDER_ID")
-	private int funding_order_id;
+	private int funding_order_id; // PK
 	
 	@ManyToOne
 	@JoinColumn(name="funding_id")
-	private Funding funding; // PK
+	private Funding funding; // FK
 	
 	@OneToOne
 	@JoinColumn(name="member_id")
