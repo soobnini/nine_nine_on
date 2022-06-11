07b447c0e29b32b1d7e296a4946c477b33a8b06e
soobnini <soobiny4339@gmail.com>
2022-06-11 14:58:29 +0900

    수정:communityService/Repository,CommentReository 생성:CommunityController

--- sharebook/src/main/java/com/sharebook/sharebook/repository/CommunityRepository.java
+++ sharebook/src/main/java/com/sharebook/sharebook/repository/CommunityRepository.java
@@ -2,16 +2,23 @@ package com.sharebook.sharebook.repository;
 
 import java.util.List;
 
+
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.sharebook.sharebook.domain.Community;
 import com.sharebook.sharebook.domain.Member;
 
-public interface CommunityRepository extends CrudRepository<Community, Integer>{
+public interface CommunityRepository extends JpaRepository<Community, Integer>{
 	List<Community> findByTitle(String title) throws DataAccessException;
 	List<Community> findByMember(Member member) throws DataAccessException;
 	List<Community> findByCategory(int category) throws DataAccessException;
+	
+	Page<Community> findAll(Pageable pagealbe)throws DataAccessException;
+	Page<Community> findAllByCategory(int category,Pageable pagealbe)throws DataAccessException;
 	
 	List<Community> findByTitleContaining(String keyword) throws DataAccessException;//키워드검색
 }
