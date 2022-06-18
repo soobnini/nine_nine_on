e515d3bd04aca5d8eab41f32dcf25f52e21c1c27
mikaniz <k515jina@gmail.com>
2022-06-18 14:47:57 +0900

    Update Funding controller, service, repository

--- sharebook/src/main/java/com/sharebook/sharebook/controller/FundingController.java
+++ sharebook/src/main/java/com/sharebook/sharebook/controller/FundingController.java
@@ -13,19 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
 
 import com.sharebook.sharebook.domain.Funding;
+import com.sharebook.sharebook.domain.Likes_funding;
+import com.sharebook.sharebook.domain.Member;
+import com.sharebook.sharebook.domain.Reward;
 import com.sharebook.sharebook.service.FundingService;
-import com.sharebook.sharebook.service.MemberService;
 
 @Controller
 public class FundingController {
 	
 	@Autowired
 	private FundingService fundingService;
-	
-	@Autowired
-	private MemberService memberService;
 
 	@GetMapping("/book/funding/create.do")
 	public String createFunding() {
@@ -50,14 +50,37 @@ public class FundingController {
 		funding.setViews(0);
 		funding.setGoal_amount(goal_amount);
 		funding.setDeadline(deadline);
-//		session으로 member 정보 얻도록 수정 필요
-		funding.setMember(memberService.getMember(1));
+		
+//		funding 등록 버튼 선택 시 session 검사하도록 수정
+		UserSession userSession = 
+				(UserSession) WebUtils.getSessionAttribute(request, "userSession");
+		funding.setMember(userSession.getMember());
 		
 		fundingService.insertFunding(funding);
 		
-//		리워드 추가 View 구현 후 리워드 insert 구현 필요
-
 		int funding_id = funding.getFunding_id();
+		
+		int reward_index = 1;
+		String reward_price = "";
+		String reward_prize = "";
+		while (reward_price != null) {
+			reward_price = request.getParameter("reward" + reward_index);
+			if (reward_price == null) {
+				continue;
+			}
+			reward_prize = request.getParameter("reward" + reward_index + "_description");
+			
+			Reward reward = new Reward();
+			reward.setPrice(Integer.parseInt(reward_price));
+			reward.setPrize(reward_prize);
+			reward.setImage("/images/ex_image.png");
+			reward.setFunding(fundingService.getFunding(funding_id));
+			
+			fundingService.insertReward(reward);
+			
+			reward_index++;
+		}
+		
 		return "redirect:/book/funding/" + funding_id + ".do";
 	}
 	
@@ -66,21 +89,66 @@ public class FundingController {
 		return new ModelAndView("fundingList", "fundingList", fundingService.getFundingList());
 	}
 
-//	findBy___Containing(String ___) : 첫 번째 시도만 정상적인 결과가 반환되고 두 번째 시도부터 오류가 발생
 	@GetMapping("/book/searchFunding/{title}.do")
 	public ModelAndView searchFunding(@PathVariable String title) {
 		return new ModelAndView("fundingList", "fundingList", fundingService.searchFundingListByTitle(title));
 	}
 	
 	@GetMapping("/book/funding/{fundingId}.do")
-	public ModelAndView detailFunding(@PathVariable int fundingId) {
+	public ModelAndView detailFunding(HttpServletRequest request, @PathVariable int fundingId) {
 		ModelAndView modelAndView = new ModelAndView("detailFunding");
 		
 		Funding funding = fundingService.getFunding(fundingId);
+		funding.setViews(funding.getViews() + 1);
+		fundingService.updateFunding(funding);
+		
+		long deadline = funding.getDeadline().getTime() / 1000 / 60 / 60 / 24;
+		long today = System.currentTimeMillis() / 1000 / 60 / 60 / 24;
+		
 		modelAndView.addObject("funding", funding);
+		modelAndView.addObject("dDay", (int) (deadline - today) + 1);
+		modelAndView.addObject("likesCount", fundingService.getLikes_fundingCount(funding));
 		modelAndView.addObject("rewardList", fundingService.getRewardList(funding));
 		
+		UserSession userSession = 
+				(UserSession) WebUtils.getSessionAttribute(request, "userSession");
+		String likes = "";
+		if (fundingService.getLikes_funding(funding, userSession.getMember()) != null) {
+			likes = "/images/filled_likes.png";
+		} else {
+			likes = "/images/blank_likes.png";
+		}
+		modelAndView.addObject("likes", likes);
+		
 		return modelAndView;
+	}
+	
+	@GetMapping("/book/funding/{fundingId}/likes.do")
+	public String likesFunding(HttpServletRequest request, @PathVariable int fundingId) {
+		Funding funding = fundingService.getFunding(fundingId);
+		
+		UserSession userSession = 
+				(UserSession) WebUtils.getSessionAttribute(request, "userSession");
+		Member member = userSession.getMember();
+		
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		System.out.println("%%%%%%%%");
+		
+		Likes_funding likes_funding = fundingService.getLikes_funding(funding, member);
+		if (likes_funding != null) {
+			fundingService.deleteLikes_funding(likes_funding);
+		} else {
+			likes_funding = new Likes_funding(member, funding);
+			fundingService.insertLikes_funding(likes_funding);
+		}
+		
+		return "redirect:/book/funding/" + fundingId + ".do";
 	}
 	
 }
--- sharebook/src/main/java/com/sharebook/sharebook/repository/FundingRepository.java
+++ sharebook/src/main/java/com/sharebook/sharebook/repository/FundingRepository.java
@@ -3,14 +3,15 @@ package com.sharebook.sharebook.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.sharebook.sharebook.domain.Funding;
 import com.sharebook.sharebook.domain.Member;
 
 public interface FundingRepository extends JpaRepository<Funding, Integer> {
-	List<Funding> findByTitleContaining(String title);
+	List<Funding> findByTitleContaining(@Param("title") String title);
 	
-	List<Funding> findByAuthorContaining(String author);
+	List<Funding> findByAuthorContaining(@Param("author") String author);
 	
 	List<Funding> findByMember(Member member);
 }
