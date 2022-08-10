6b6b8dcfe0b32ed0de0a9de328c323f5218f54e0
LAPTOP-D5OGQ982\EUNDINI <lee08236@naver.com>
2022-08-10 20:38:12 +0900

    Fix Genre Function

--- sharebook/src/main/java/com/sharebook/sharebook/repository/GenreRepository.java
+++ sharebook/src/main/java/com/sharebook/sharebook/repository/GenreRepository.java
@@ -2,11 +2,10 @@ package com.sharebook.sharebook.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.sharebook.sharebook.domain.Book;
 import com.sharebook.sharebook.domain.Genre;
 
-public interface GenreRepository extends JpaRepository<Genre, Integer> {
-	List<Genre> findByBook(Book book);
+public interface GenreRepository extends CrudRepository<Genre, Integer> {
 }
