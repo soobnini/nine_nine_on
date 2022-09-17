49236d6e452ef2b89c3b399fcf5feedefe48da24
gs25q <rs13q5@gmail.com>
2022-09-17 16:26:04 +0900

    수정 : 카테고리 및 검색조건 수정

--- sharebook/src/main/resources/static/js/bookList.js
+++ sharebook/src/main/resources/static/js/bookList.js
@@ -21,9 +21,81 @@ $("#recent-sort").click(function(e) {
 	loadAjaxFromAPI("3", "/book/list/sort.do", "POST", "#book-list-result");
 });
 
+/**
+ * 검색조건 설정
+ */
+$('label[class^=condition-label-region]').unbind("click").on("click", function(e) {
+	e.preventDefault();
+
+	$(this).css("display", "none");
+	$(this).next().css("display", "inline");
+	$(this).next().children(".condition-checkbox").attr("checked", true);
+
+	var checkedList = [];
+	$("input:checkbox[name=addressCondition]:checked").each(function(i) {
+		checkedList.push($(this).val());
+	});
+	console.log(checkedList);
+
+	loadAjaxAboutArray(checkedList, "/book/list/condition/region.do", "POST", "#book-list-result");
+});
+
+$('label[class^=condition-label-genre]').unbind("click").on("click", function(e) {
+	e.preventDefault();
+
+	$(this).css("display", "none");
+	$(this).next().css("display", "inline");
+	$(this).next().children(".condition-checkbox").attr("checked", true);
+
+	var checkedList = [];
+	$("input:checkbox[name=genreCondition]:checked").each(function(i) {
+		checkedList.push($(this).val());
+	});
+	console.log(checkedList);
+
+	loadAjaxAboutArray(checkedList, "/book/list/condition/genre.do", "POST", "#book-list-result");
+});
+
+$('label[class^=condition-label-region-checked]').unbind("click").on("click", function(e) {
+	e.preventDefault();
+	console.log($(this));
+	var conditionValue = $(this).children(".condition-checkbox").val();
+
+	$(this).css("display", "none");
+	$(this).prev().css("display", "inline");
+	$(this).children(".condition-checkbox").attr("checked", false);
+
+	var checkedList = [];
+	$("input:checkbox[name=addressCondition]:checked").each(function(i) {
+		checkedList.push($(this).val());
+	});
+	console.log(checkedList);
+
+	loadAjaxAboutArray(checkedList, "/book/list/condition/region.do", "POST", "#book-list-result");
+});
+
+$('label[class^=condition-label-genre-checked]').unbind("click").on("click", function(e) {
+	e.preventDefault();
+	console.log($(this));
+	var conditionValue = $(this).children(".condition-checkbox").val();
+
+	$(this).css("display", "none");
+	$(this).prev().css("display", "inline");
+	$(this).children(".condition-checkbox").attr("checked", false);
+
+	var checkedList = [];
+	$("input:checkbox[name=genreCondition]:checked").each(function(i) {
+		checkedList.push($(this).val());
+	});
+	console.log(checkedList);
+
+	loadAjaxAboutArray(checkedList, "/book/list/condition/genre.do", "POST", "#book-list-result");
+});
+
+/*
 $(".condition-label.region").click(function() {
 	console.log($(this));
-	var conditionValue = $(this).find(".condition-checkbox").val();
+	var conditionValue = $(this).children(".condition-checkbox").val();
 	
 	$(this).css("display", "none");
 	$(this).next().css("display", "inline");
@@ -33,10 +105,11 @@ $(".condition-label.region").click(function() {
 
 $(".condition-label.genre").click(function() {
 	console.log($(this));
-	var conditionValue = $(this).find(".condition-checkbox").val();
+	var conditionValue = $(this).children(".condition-checkbox").val();
 	
 	$(this).css("display", "none");
 	$(this).next().css("display", "inline");
 	
 	loadAjaxFromAPI(conditionValue, "/book/list/condition/genre.do", "POST", "#book-list-result");
 }); 
+*/
